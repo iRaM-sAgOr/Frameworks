b5bb9c88712b6b4abedc566bc41bd1961873ae3f
iRaM-sAgOr <ikramulhaqsagor@gmail.com>
2024-10-07 15:38:03 +0600

    Model loading with lifespan and Docker file add

--- FastAPI_ML/main.py
+++ FastAPI_ML/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-import uvicorn
+from contextlib import asynccontextmanager
 import joblib
 import pandas as pd
 from schema import Input
 
-app = FastAPI()
-model = joblib.load('ml_models/model.pkl')
-vectorizer = joblib.load('ml_models/vectorizer.pkl')
+ml_models = {}
+
+@asynccontextmanager
+async def get_model(app: FastAPI):
+    model = joblib.load('ml_models/model.pkl')
+    vectorizer = joblib.load('ml_models/vectorizer.pkl')
+    ml_models['model'] = model
+    ml_models['vectorizer'] = vectorizer
+    yield
+    ml_models.clear()
+
+app = FastAPI(lifespan=get_model)
 
 
 @app.get('/')
@@ -16,11 +25,12 @@ async def healthcheck():
 
 @app.post('/api/v1/spam/predict')
 async def prediction(body: Input):
-    vectorized_text = vectorizer.transform(pd.Series(body.input))
-    result = round(model.predict_proba(vectorized_text)[0][1], 2)
+    vectorized_text = ml_models['vectorizer'].transform(pd.Series(body.text))
+    result = round(ml_models['model'].predict_proba(vectorized_text)[0][1], 2)
     is_spam = True if result > .4 else False
     return {"is_spam": is_spam, "probability": result}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
